ffe25a6c1620264dc90ddb300e3d17e357428182
leipriets <leipriets@gmail.com>
2021-03-05 13:47:11 +0800

    update email configuration and production ready

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :address => ENV['SMTP_ADDRESS'],
+    :port => '587',
+    :authentication => :plain,
+    :user_name => ENV['SMTP_USER_NAME'],
+    :password => ENV['SMTP_PASSWORD'],
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
+  }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,14 +5,7 @@ Rails.application.configure do
   config.cache_classes = true
   
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    port: 587,
-    address: ENV['SMTP_ADDRESS'],
-    user_name: ENV['SMTP_USER_NAME'],
-    password: ENV['SMTP_PASSWORD'],
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  config.action_mailer.default_url_options = { :host => 'lei-photo-app.herokuapp.com', :protocol => 'https' }
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
