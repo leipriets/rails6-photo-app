146451c55df34095e0bccff089c4bf5037266b71
leipriets <leipriets@gmail.com>
2021-03-05 14:48:47 +0800

    production ready

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,7 @@ module PhotoApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.assets.initialize_on_precompile = false
-    config.after_initialize do
-    config.action_mailer.perform_deliveries    = true
+    config.action_mailer.perform_deliveries = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,7 @@ Rails.application.configure do
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'lei-photo-app.herokuapp.com', :protocol => 'https' }
+  config.action_mailer.perform_deliveries = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -67,7 +68,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
